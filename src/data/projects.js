a815b871429e287caff7623452c8fbd01441cfd7
Lê Thanh Thương <92387369+thanhthuongle@users.noreply.github.com>
2026-06-10 16:34:52 +0700

    Add Lottie animation for Trello background (#3)
    - Introduced a new JSON file containing Lottie animation data for the Trello background.
    - The animation includes multiple layers with transformations and shapes to enhance visual appeal.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,6 @@
 import heyMoneyBg from "../assets/hey-money-bg.json";
+import trelloBg from "../assets/trello-bg.json";
+import educationBg from "../assets/graduation-cap.json";
 import Lottie from "lottie-react";
 
 
@@ -13,42 +15,42 @@ export const projects = [
       en: 'A website that helps users manage personal finances with features such as recording and retrieving income/expense transactions, managing multiple accounts, supporting savings and accumulation tracking, debt management with interest rates, and setting spending limits by category...',
     },
     animation: heyMoneyBg,
-    image: '/projects/project-1.jpg',
+    image: '',
     tech: ['ReactJS', 'NodeJS (ExpressJS)', 'Redux', 'Material UI', 'MongoDB', 'Socket.IO', 'JWT'],
     demo: 'https://hey-money.vercel.app',
     source: 'https://github.com/thanhthuongle/Personal-Finance-Management-Website-Development',
     featured: true,
   },
-
-  // TODO: Hoàn thiện thêm thông tin
-  // {
-  //   title: {
-  //     vi: 'Hệ thống Quản lý Tài chính',
-  //     en: 'Finance Management',
-  //   },
-  //   description: {
-  //     vi: 'Giao diện cửa hàng trực tuyến với giỏ hàng, thanh toán và quản lý đơn hàng.',
-  //     en: 'An online store front with cart, checkout, and order management.',
-  //   },
-  //   image: '/projects/project-2.jpg',
-  //   tech: ['React', 'Tailwind', 'Node.js'],
-  //   demo: 'https://example.com',
-  //   source: 'https://github.com/yourname/project-2',
-  //   featured: false,
-  // },
-  // {
-  //   title: {
-  //     vi: 'Hệ thống Quản lý Tài chính',
-  //     en: 'Finance Management',
-  //   },
-  //   description: {
-  //     vi: 'Dự báo thời tiết theo vị trí, giao diện tối giản và hoạt ảnh mượt.',
-  //     en: 'Location-based weather forecast with a minimal UI and smooth animations.',
-  //   },
-  //   image: '/projects/project-3.jpg',
-  //   tech: ['React Native', 'TypeScript', 'OpenWeather API'],
-  //   demo: '',
-  //   source: 'https://github.com/yourname/project-3',
-  //   featured: false,
-  // },
+  {
+    title: {
+      vi: 'Trello Clone',
+      en: 'Trello Clone',
+    },
+    description: {
+      vi: 'Xây dựng một dự án giống Trello cho phép quản lý công việc với giao diện kéo thả và cộng tác thời gian thực sử dụng Socket.IO, củng cố kỹ năng React và NodeJS.',
+      en: 'Built a Trello-like project enabling task management with drag-and-drop UI and real-time collaboration using Socket.IO, reinforcing my skills in React and NodeJS.',
+    },
+    animation: trelloBg,
+    image: '',
+    tech: ['React', 'Redux', 'Material UI', 'Dndkit', 'NodeJS (ExpressJS)', 'MongoDB', 'Socket.IO'],
+    demo: 'https://trello-web-beta-three.vercel.app',
+    source: 'https://github.com/thanhthuongle/Trello-like-Website-Development',
+    featured: false,
+  },
+  {
+    title: {
+      vi: 'Ứng dụng quản lý trường đại học',
+      en: 'University Management App',
+    },
+    description: {
+      vi: 'Hợp tác với nhóm để phát triển giao diện ứng dụng di động cho sinh viên và giảng viên quản lý các hoạt động học tập, bao gồm lịch học, bài tập, điểm danh và thông báo...',
+      en: 'Collaborated with a team to develop a mobile app interface for students and lecturers to manage learning activities, including class schedules, assignments, attendance, and notifications...',
+    },
+    animation: educationBg,
+    image: '',
+    tech: ['React Native', 'TypeScript', 'Expo', 'Expo Router'],
+    demo: '',
+    source: 'https://github.com/NguyenHiep2003/QLDT',
+    featured: false,
+  },
 ];
